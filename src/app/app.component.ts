358c244f78602fe60c3a27b5c2a7c3227f53af20
admin <admin@Walkss-MacBook-Pro-2.local>
2022-09-15 12:57:15 -0500

    completed project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-drop-app';
+  midshift = [
+    '1pm - 2pm',
+    '2pm - 3pm'
+  ];
+  dayshift = [
+    '6am - 7am',
+    '7am - 8am'
+  ];
+  eveshift = [
+    '9pm - 10pm ',
+    '10pm - 11pm'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log('from: ' + event.previousContainer.id + ' to: ' + event.container.id);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 }
